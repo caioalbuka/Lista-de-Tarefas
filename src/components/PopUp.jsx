ba782c0bffa15113fd9e46165e3b33c02dba4dbf
Caio Albuka <caioalbuka@Leal.local>
2021-12-13 17:58:49 -0300

    Modal a Mostra

---
+++ src/components/PopUp.jsx
@@ -0,0 +1,25 @@
+import React, { useRef } from 'react';
+import ReactDom from 'react-dom';
+import Button from './Button';
+
+export const Modal = ({ setShowModal }) => {
+  // close the modal when clicking outside the modal.
+  const modalRef = useRef();
+  const closeModal = (e) => {
+    if (e.target === modalRef.current) {
+      setShowModal(true);
+    }
+  };
+  //render the modal JSX in the portal div.
+  return ReactDom.createPortal(
+    <div className="container" ref={modalRef} onClick={closeModal}>
+      <div className="modal">
+        <h2>Isto é um PopUp</h2>
+        <Button onClick={() => setShowModal(true)}>Voltar</Button>
+        <Button onClick={() => setShowModal(false)}>Ok</Button>
+      </div>
+    </div>,
+    document.getElementById('portal'),
+  );
+};
+export default Modal;
